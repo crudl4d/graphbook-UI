153dd819f95dfd642371299c07595dfbcd86ecf3
Damian Kaczmarczyk <damiankaczmarczyk0@gmail.com>
2022-10-15 00:39:15 +0200

    handle auth

--- app/src/main/java/com/example/dogebook/data/LoginDataSource.kt
+++ app/src/main/java/com/example/dogebook/data/LoginDataSource.kt
@@ -1,6 +1,8 @@
 package com.example.dogebook.data
 
 import com.example.dogebook.data.model.LoggedInUser
+import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import java.io.IOException
 
 /**
@@ -8,11 +10,29 @@ import java.io.IOException
  */
 class LoginDataSource {
 
+    private val client: OkHttpClient = OkHttpClient()
+    private val BASE_URL = "http://192.168.0.101:8080"
+
     fun login(username: String, password: String): Result<LoggedInUser> {
         try {
-            // TODO: handle loggedInUser authentication
-            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
-            return Result.Success(fakeUser)
+            val credential = Credentials.basic(username, password)
+            val url = ("$BASE_URL/users/login/").toHttpUrl().newBuilder()
+                .build().toString()
+
+            val request: Request = Request.Builder()
+                .url(url)
+                .addHeader("Authorization", credential)
+                .addHeader("Connection", "keep-alive")
+                .addHeader("Accept-Encoding", "gzip, deflate, br")
+                .addHeader("Accept", "application/json")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("withCredentials", "true")
+                .addHeader("User-Agent", "PostmanRuntime/7.29.2")
+                .build()
+            val call: Call = client.newCall(request)
+            val response: Response = call.execute()
+            val user = LoggedInUser(response.header("id", "")!!, response.header("name", "")!!)
+            return Result.Success(user)
         } catch (e: Throwable) {
             return Result.Error(IOException("Error logging in", e))
         }
--- app/src/main/java/com/example/dogebook/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/dogebook/ui/login/LoginViewModel.kt
@@ -4,10 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import android.util.Patterns
+import androidx.lifecycle.viewModelScope
 import com.example.dogebook.data.LoginRepository
 import com.example.dogebook.data.Result
 
 import com.example.dogebook.R
+import com.example.dogebook.data.model.LoggedInUser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
 
@@ -19,10 +23,13 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
 
     fun login(username: String, password: String) {
         // can be launched in a separate asynchronous job
-        val result = loginRepository.login(username, password)
+        var result: Result<LoggedInUser>? = null
+        viewModelScope.launch(Dispatchers.IO) {
+            result = loginRepository.login(username, password)
+        }
 
         if (result is Result.Success) {
-            _loginResult.value = LoginResult(success = LoggedInUserView(displayName = result.data.displayName))
+            _loginResult.value = LoginResult(success = LoggedInUserView(displayName = (result as Result.Success<LoggedInUser>).data.displayName))
         } else {
             _loginResult.value = LoginResult(error = R.string.login_failed)
         }
@@ -49,6 +56,6 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
 
     // A placeholder password validation check
     private fun isPasswordValid(password: String): Boolean {
-        return password.length > 5
+        return true//password.length > 5
     }
 }
