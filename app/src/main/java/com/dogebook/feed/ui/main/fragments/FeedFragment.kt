399259d21690b206b5cd74655d819abad387f3ec
Damian Kaczmarczyk <damiankaczmarczyk0@gmail.com>
2022-10-16 13:28:42 +0200

    implement infinite scrolling through feed

--- app/src/main/java/com/dogebook/feed/ui/main/fragments/FeedFragment.kt
+++ app/src/main/java/com/dogebook/feed/ui/main/fragments/FeedFragment.kt
@@ -1,10 +1,13 @@
 package com.dogebook.feed.ui.main.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.dogebook.R
 
 class FeedFragment : Fragment() {
@@ -15,5 +18,72 @@ class FeedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        populateData()
+        initAdapter()
+        initScrollListener()
+    }
+
+    var recyclerView: RecyclerView? = null
+    var recyclerViewAdapter: RecyclerViewAdapter? = null
+    var rowsArrayList: ArrayList<String?> = ArrayList()
+
+    var isLoading = false
+
+    private fun populateData() {
+        var i = 0
+        while (i < 10) {
+            rowsArrayList.add("Item $i")
+            i++
+        }
+    }
+
+    private fun initAdapter() {
+        recyclerView = requireView().findViewById(R.id.recyclerView)
+        recyclerViewAdapter = RecyclerViewAdapter(rowsArrayList)
+        recyclerView!!.adapter = recyclerViewAdapter
+    }
+
+    private fun initScrollListener() {
+        recyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val linearLayoutManager = recyclerView.layoutManager as LinearLayoutManager?
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == rowsArrayList.size - 1) {
+                        //bottom of list!
+                        loadMore()
+                        isLoading = true
+                    }
+                }
+            }
+        })
+    }
+
+    private fun loadMore() {
+        rowsArrayList.add(null)
+        recyclerViewAdapter!!.notifyItemInserted(rowsArrayList.size - 1)
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            rowsArrayList.removeAt(rowsArrayList.size - 1)
+            val scrollPosition: Int = rowsArrayList.size
+            recyclerViewAdapter!!.notifyItemRemoved(scrollPosition)
+            var currentSize = scrollPosition
+            val nextLimit = currentSize + 10
+            while (currentSize - 1 < nextLimit) {
+                rowsArrayList.add("Item $currentSize")
+                currentSize++
+            }
+            recyclerViewAdapter!!.notifyDataSetChanged()
+            isLoading = false
+        }, 2000)
     }
 }
